0dc24560ac9cee5f614867c8034098218bd043fb
Diana Suasnavas <diana.suasnavas@thoughtworks.com>
2024-05-20 12:12:41 -0500

    fin de Seccion 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'basis';
+  public title: string = 'Mi primera app de Angular';
 }
